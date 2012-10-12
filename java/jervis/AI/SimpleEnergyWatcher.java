63150d897adb3e87d4cff3c783d50b0aca512dcc
Kalman Tarnay <kalmisoft@gmail.com>
2012-10-12 18:33:30 +0200

    dumb fixes for energy watcher against multiple enemies (it is actually kind of broken now, but works good enough)

--- java/jervis/AI/SimpleEnergyWatcher.java
+++ java/jervis/AI/SimpleEnergyWatcher.java
@@ -1,24 +1,19 @@
 package jervis.AI;
 
-import jervis.CommonTypes.Perception;
 
 public class SimpleEnergyWatcher {
 	static Integer otherTeamCollectiveEnergyLastRound = null;
-	static Integer simpleId = null;
 	static boolean simpleAteLastRound = false;
 	
 	static boolean run(Perception p, State state){		
-		if(simpleId == null && p.time > 1){
-			simpleId = determineSimpleId(state);
-		}
 		
 		if(p.time > 1){
 			int otherTeamCollectiveEnergy = getSimpleEnergy(p, state);
 			if(otherTeamCollectiveEnergy<100) return false;
-			if(p.internalId == (simpleId+1) % 5){
+			if(p.internalId == 0){
 				Agent agent = state.getAgent(p.internalId);
 				
-				if(p.time > 2){
+				if(p.time > 2 && otherTeamCollectiveEnergyLastRound != null){
 					boolean simpleAteThisRound = (otherTeamCollectiveEnergy>otherTeamCollectiveEnergyLastRound+6);
 					
 					if(simpleAteThisRound) Stat.logSimpleEat();
@@ -37,20 +32,6 @@ public class SimpleEnergyWatcher {
 		return true;
 	}
 
-	private static int determineSimpleId(State state) {
-		int id = 5;
-		for (Agent agent : state.agents) {
-			if(agent.order>agent.id){
-				id = agent.order-1;
-				break;
-			}
-		}
-		return id;
-	}
-	
-	public static boolean isSimpleAlive(Perception p, State state){
-		return !(getSimpleEnergy(p, state) < 100);
-	}
 	
 	private static int getSimpleEnergy(Perception p, State state){
 		int myCollectiveEnergy = Agent.getCollectiveEnergy(state.agents);

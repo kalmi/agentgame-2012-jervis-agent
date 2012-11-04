292764a1e497243527666a63fd8ebd58aa77fc8f
Kalman Tarnay <kalmisoft@gmail.com>
2012-11-04 14:36:14 +0100

    add canSee to PerceivedAgent

--- java/jervis/CommonTypes/PerceivedAgent.java
+++ java/jervis/CommonTypes/PerceivedAgent.java
@@ -27,4 +27,36 @@ public class PerceivedAgent extends Point {
 		this.id = id;
 	}
 
+	
+	
+	
+    private boolean isFacing(int x, int y) {
+        int A = this.x + this.y, B = this.y - this.x,
+                C = x + y, D = y - x;
+
+        if (dir == MyDir.up) {
+            return C <= A && D <= B;
+        } else if (dir == MyDir.right) {
+            return C >= A && D <= B;
+        } else if (dir == MyDir.down) {
+            return C >= A && D >= B;
+        } else {
+            return C <= A && D >= B;
+        }
+	}
+    
+	public boolean isFacing(Point pos) {
+		return isFacing(pos.x, pos.y);
+    }
+
+	public boolean canSee(int x, int y) {
+        if (Math.abs(this.x - x) > 10 || Math.abs(this.y - y) > 10) {
+            return false;
+        }
+        return isFacing(x,y); 
+	}
+
+	public boolean canSee(Point pos) {
+    	return canSee(pos.x, pos.y);
+    }
 }
